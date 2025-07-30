4cbac5cb8e2c47b383a20ebebf554b5952b77c1d
Isadean <isadora.alves.andrade@escola.pr.gov.br>
2025-07-30 12:52:56 +0000

    criamos as perguntas no javascript

---
+++ perguntas.js
@@ -0,0 +1,18 @@
+criaCartao(
+    'Quimica',
+    "O que é uma reação de oxidação-redução?",
+    " há transferêcia de elétrons entre os reagentes."
+)
+criaCartao(
+    " Matematica",
+    " O que define uma função linear e como representar?",
+    "Representado por f(x)=ax+b,tem linha reta."
+)
+criaCartao(
+    "Fisica",
+    "Como calcular a aceleração da segunda Lei de Newton?",
+    "A aceleração de um objeto é proporcional a sua massa."
+)
+criaCartao(
+    "Fisica"
+)
