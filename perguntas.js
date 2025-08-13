4b7aca05cf16996ad280044d201073369563e330
Isadean <isadora.alves.andrade@escola.pr.gov.br>
2025-08-13 11:15:16 +0000

    perguntas

--- perguntas.js
+++ perguntas.js
@@ -14,5 +14,37 @@ criaCartao(
     "A aceleração de um objeto é proporcional a sua massa."
 )
 criaCartao(
-    "Fisica"
+    "Fisica",
+    'O tempo é linear?',
+    'Sim, o tempo é geralmente considerado linear na nossa experiência cotidiana e na física clássica, seguindo uma direção única do passado para o futuro.'
+)
+criaCartao(
+    'Geografia',
+    'Qual é o único continente com terra nos quatro hemisférios?',
+    'O único continente com terra nos quatro hemisférios da Terra é a África. '
+)
+criaCartao(
+    'Geogrfia',
+    'Em que país se encontra o Salto Ángel, a maior cachoeira do mundo? ',
+    'O Salto Angel, a maior cachoeira do mundo, está localizada na Venezuela.'
+)
+criaCartao(
+    ' Português',
+    'Pergunta: Qual é o coletivo de "alunos"?',
+    'Resposta: Classe ou turma.'
+)
+criaCartao(
+    'Português',
+    'Qual é o sujeito da frase: "A menina brinca no jardim"?',
+    'A menina.'
+)
+criaCartao(
+    'Português',
+    'Qual a diferença entremal e mau?',
+'"Mal" é o contrário de "bem", e "mau" é o contrário de "bom".',
+)
+criaCartao(
+    'Português',
+    'Qual o tempo verbal da frase: "Eu comerei o bolo"?',
+' Futuro do presente.'
 )
